5a5b8ac7a83f82a6eebbbc5c180f91db1483afc3
Эрк Рауф Эльдарович <rerk@phoenixit.ru>
2022-06-26 22:46:14 +0300

    initCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo, useState, useEffect } from 'react';
 
-function App() {
+const protoAPI = 'http://194.67.78.219:8080/';
+
+// {
+//   "externalId": "1",
+//   "name": "Реестровое дело лесничества"
+// },
+
+function AppContainer() {
+  const [listArr, setListArr] = useState(null);
+
+  const getCaseesList = async () => {
+    try {
+      const response = await fetch(protoAPI, {
+        method: 'GET',
+        cors: 'no-cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) throw new Error('NOT ok!!!!');
+      const result = await response.json();
+      setListArr(result);
+    } catch (error) {
+      console.log('ERROR===>');
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getCaseesList();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      <h1>Реестровыые Дела:</h1>
+      <ol>
+        {listArr &&
+          listArr.map(({ externalId, name }) => (
+            <li key={externalId}>{name}</li>
+          ))}
+      </ol>
+    </p>
   );
 }
 
-export default App;
+export const App = memo(AppContainer);
